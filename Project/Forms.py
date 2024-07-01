6963e7791495ceca1bca8814c574906e81e5df97
YungYan <laiyungyan@gmail.com>
2024-07-01 14:13:38 +0800

    Add files via upload

--- Project/Forms.py
+++ Project/Forms.py
@@ -1,11 +1,11 @@
 from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators, PasswordField, ValidationError
 from wtforms.fields import EmailField, DateField
 import shelve
-from flask import session
+from flask import Flask
 
 
 class CreateUserForm(Form):
-    username = StringField('Username', [validators.Length(min=1, max=20), validators.DataRequired()])
+    username = StringField('Username', [validators.Length(min=1, max=30), validators.DataRequired()])
     email = EmailField('Email Address', [validators.Length(min=1, max=50), validators.DataRequired(), validators.email("Please enter a valid email address")])
     password = PasswordField('Password', [validators.Length(min=8, max=30), validators.DataRequired(), validators.equal_to('confirm', "Passwords must match")])
     confirm = PasswordField('Confirm Password')
@@ -43,7 +43,6 @@ class newusername(Form):
 
 class newphone(Form):
     phone = StringField('New Phone Number', [validators.Length(min=8, max=8), validators.DataRequired()])
-
     def validate_phone(form, phone):
         try:
             int(phone.data)
@@ -65,3 +64,4 @@ class newemail(Form):
 
 class deleteaccount(Form):
     password_check = PasswordField('Enter Password to confirm', [validators.DataRequired()])
+
