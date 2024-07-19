ca1630b35c22ec1f1c4af77929fc4c2eaf1f6247
samuel-3110 <151658309+samuel-3110@users.noreply.github.com>
2024-07-20 03:44:42 +0800

    Update Forms.py
    - added itemform and its custom category validation

--- Project/Forms.py
+++ Project/Forms.py
@@ -1,10 +1,24 @@
-from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators, PasswordField, ValidationError
-from wtforms.fields import EmailField, DateField
-import shelve
-from flask import Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, DecimalField, TextAreaField, SelectField, RadioField, SubmitField, validators, EmailField, PasswordField
+from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
+from flask_wtf.file import MultipleFileField
 
 
-class CreateUserForm(Form):
+def validate_category(form, field):
+    if field.data == "Select a category":
+        raise ValidationError("Please select a valid category.")
+
+class ItemForm(FlaskForm):
+    images = MultipleFileField("Images", validators=[DataRequired()])
+    name = StringField("Listing Title", validators=[DataRequired(), Length(max=100)])
+    category = SelectField("Category", choices=[("Select a category", "Select a category"),("Beauty & Personal Care", "Beauty & Personal Care"), ("Computers & Tech", "Computers & Tech"), ("Everything Else", "Everything Else"), ("Food & Drinks", "Food & Drinks"), ("Furniture & Home Living", "Furniture & Home Living"), ("Health & Nutrition", "Health & Nutrition"), ("Men's Fashion", "Men's Fashion"), ("Women's Fashion", "Women's Fashion")], validators=[DataRequired(), validate_category])
+    condition = RadioField("Condition", choices=[("Brand new", "Brand new"), ("Like new", "Like new"), ("Lightly used", "Lightly used"), ("Well used", "Well used"), ("Heavily used", "Heavily used")], validators=[DataRequired()])
+    price = DecimalField("Price of your listing", validators=[DataRequired(), NumberRange(min=0)])
+    description = TextAreaField("Description", validators=[Length(max=500)])
+    submit = SubmitField("Submit")
+
+
+class CreateUserForm(FlaskForm):
     username = StringField('Username', [validators.Length(min=1, max=30), validators.DataRequired()])
     email = EmailField('Email Address', [validators.Length(min=1, max=50), validators.DataRequired(), validators.email("Please enter a valid email address")])
     password = PasswordField('Password', [validators.Length(min=8, max=30), validators.DataRequired(), validators.equal_to('confirm', "Passwords must match")])
@@ -18,30 +32,30 @@ class CreateUserForm(Form):
             raise ValidationError('Please enter a valid phone number')
 
 
-class UserLogin(Form):
+class UserLogin(FlaskForm):
     email_address_check = StringField('Email Address', [validators.DataRequired()])
     password_check = PasswordField('Password', [validators.DataRequired()])
 
 
-class forgotpassword(Form):
+class forgotpassword(FlaskForm):
     email_address_check = StringField('Email Address', [validators.DataRequired()])
 
 
-class forgotreset(Form):
+class forgotreset(FlaskForm):
     security = StringField('Security Code', [validators.DataRequired()])
     password = PasswordField('New Password', [validators.Length(min=8, max=30), validators.DataRequired(), validators.equal_to('confirm', "Passwords must match")])
     confirm = PasswordField('Confirm Password')
 
 
-class newaddress(Form):
+class newaddress(FlaskForm):
     mailing_address = StringField('New Mailing Address', [validators.Length(min=1, max=150), validators.DataRequired()])
 
 
-class newusername(Form):
+class newusername(FlaskForm):
     username = StringField('New Username', [validators.Length(min=1, max=20), validators.DataRequired()])
 
 
-class newphone(Form):
+class newphone(FlaskForm):
     phone = StringField('New Phone Number', [validators.Length(min=8, max=8), validators.DataRequired()])
     def validate_phone(form, phone):
         try:
@@ -50,18 +64,17 @@ class newphone(Form):
             raise ValidationError('Please enter a valid phone number')
 
 
-class newpassword(Form):
+class newpassword(FlaskForm):
     password_check = PasswordField('Old Password', [validators.DataRequired()])
     password = PasswordField('New Password', [validators.Length(min=8, max=30), validators.DataRequired(), validators.equal_to('confirm', "Passwords must match")])
     confirm = PasswordField('Confirm Password')
 
 
-class newemail(Form):
+class newemail(FlaskForm):
     password_check = PasswordField('Password', [validators.DataRequired()])
     email = EmailField('New Email Address', [validators.Length(min=1, max=50), validators.DataRequired(),
                                          validators.email("Please enter a valid email address")])
 
 
-class deleteaccount(Form):
+class deleteaccount(FlaskForm):
     password_check = PasswordField('Enter Password to confirm', [validators.DataRequired()])
-
